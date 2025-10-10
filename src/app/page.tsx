3d39f3d2ad5bb0e0c6500d352c9b366baf1b9b0c
Musfiq Rahman <procreationsbliss@gmail.com>
2025-10-10 13:30:47 +0600

    Popular city section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/hero/Hero";
+import { PopularCityList } from "@/components/popular-city-list/popular-city-list";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <PopularCityList />
     </div>
   );
 }
